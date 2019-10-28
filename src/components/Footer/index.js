e80f5dc5ec419e9efb04d9c30940f58f5eee8d1b
ArianeSpa <ariane.spanneut@gmail.com>
2019-10-28 08:24:04 +0100

    icon footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,6 @@
 // == Import : npm
 import React from 'react';
-import {
-  Segment, Button,
-} from 'semantic-ui-react';
+import { Segment, Button, Container } from 'semantic-ui-react';
 
 // == Import : local
 import './footer.scss';
@@ -10,55 +8,46 @@ import './footer.scss';
 // == Composant
 const Footer = () => (
   <Segment className="footerSegment">
-    <p className="copyright">&copy; O'Feel.2019</p>
-    <Button
-      circular
-      color="facebook"
-      icon="facebook"
-      href="https://www.facebook.com/Oclock.io/"
-      target="_blank"
-    />
-    <Button
-      circular
-      color="twitter"
-      icon="twitter"
-      href="https://twitter.com/Oclock_io?s=17"
-      target="_blank"
-    />
-    <Button
-      circular
-      color="red"
-      icon="youtube"
-      href="https://www.youtube.com/channel/UCVdtXmsbmewiS6N9QjO8LKA"
-      target="_blank"
-    />
-    <Button
-      circular
-      color="linkedin"
-      icon="linkedin"
-      href="https://www.linkedin.com/in/ariane-spanneut/"
-      target="_blank"
-    />
-    <Button
-      circular
-      color="black"
-      icon="github"
-      href="https://github.com/ArianeSpa"
-      target="_blank"
-    />
-    <Button
-      circular
-      color="pink"
-      icon="instagram"
-      href="https://www.instagram.com/suis_ton_fil/"
-      target="_blank"
-    />
-    <Button
-      circular
-      color="yellow"
-      icon="paper plane"
-      href="mailto:ariane.spanneut@gmail.com?subject=contact%20from%20Ofeel"
-    />
+    <Container className="copyright">
+      <p>&copy; O'Feel.2019</p>
+    </Container>
+    <Container className="buttons">
+      <Button
+        icon="facebook"
+        href="https://www.facebook.com/Oclock.io/"
+        target="_blank"
+      />
+      <Button
+        icon="twitter"
+        href="https://twitter.com/Oclock_io?s=17"
+        target="_blank"
+      />
+      <Button
+        color="red"
+        icon="youtube"
+        href="https://www.youtube.com/channel/UCVdtXmsbmewiS6N9QjO8LKA"
+        target="_blank"
+      />
+      <Button
+        icon="linkedin"
+        href="https://www.linkedin.com/in/ariane-spanneut/"
+        target="_blank"
+      />
+      <Button
+        icon="github"
+        href="https://github.com/ArianeSpa"
+        target="_blank"
+      />
+      <Button
+        icon="instagram"
+        href="https://www.instagram.com/suis_ton_fil/"
+        target="_blank"
+      />
+      <Button
+        icon="mail outline"
+        href="mailto:ariane.spanneut@gmail.com?subject=contact%20from%20Ofeel"
+      />
+    </Container>
   </Segment>
 );
 
