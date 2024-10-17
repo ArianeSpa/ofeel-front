feacfb8f5b0169b59aeac9a2186c43da04a96d94
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-10-17 14:19:29 +0200

    feat: create Button component atom

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode } from "react";
+
+import { StyledButton, StyledButtonProps } from "./Button.style";
+
+export type ButtonProps = {
+  children?: ReactNode | string;
+  type?: "button" | "submit" | "reset";
+  onClick: () => void;
+} & Omit<StyledButtonProps, "theme">;
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    onClick,
+    children,
+    size,
+    width,
+    type = "button",
+    ...styledProps
+  } = props;
+
+  return (
+    <StyledButton
+      type={type}
+      size={size}
+      width={width}
+      onClick={onClick}
+      {...styledProps}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export { Button, type StyledButtonProps };
