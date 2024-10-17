1c2aa956bab703e6810bee6d12aa3235b6b57c1a
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-10-17 14:24:19 +0200

    refactor: move DashboardTag, LougoutButton and ProfileActivity to molecule components

--- src/components/molecules/LogoutButton/LogoutButton.tsx
+++ src/components/molecules/LogoutButton/LogoutButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Button } from "semantic-ui-react";
 
 // == Import : local
-import { useAppDistpatch } from "@/app/hooks";
+import { useAppDistpatch } from "@/hooks/store.hook";
 import { logOut } from "@/store/reducers/user.slice";
 import "./logout_button.scss";
 
