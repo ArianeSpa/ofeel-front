c75a6cbee45172f4a1ba4f59642319f4511a9b8a
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-10-21 14:42:37 +0200

    feat: add i18next and react-i18next

--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
 import { ThemeProvider } from "styled-components";
-import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 
 // == Import: local
 import { store } from "./store/store";
 import { App } from "./App";
-import "./index.css";
 import { mainTheme } from "./theme/theme";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+i18next.init({
+  interpolation: { escapeValue: false }, // React already does escaping
+});
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <I18nextProvider i18n={i18next}>
+      <BrowserRouter>
+        <ThemeProvider theme={mainTheme}>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </I18nextProvider>
   </React.StrictMode>
 );
 
