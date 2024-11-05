339c3ab91838e7f9337b98bcba75b4a50d1291b7
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-11-05 11:12:49 +0100

    feat: create gutters file to generate simply margin and padding styles

---
+++ src/theme/gutters.ts
@@ -0,0 +1,128 @@
+type GetGutterStyleProps = {
+  position: "top" | "right" | "bottom" | "left";
+  type: "margin" | "padding";
+  value: number;
+};
+const getGutterStyle = ({ position, type, value }: GetGutterStyleProps) => {
+  const pixelBase = value * 4;
+
+  return `${type}-${position}: ${pixelBase}px;`;
+};
+
+type GetMarginStyleProps = {
+  m?: number;
+  mx?: number;
+  my?: number;
+  mt?: number;
+  mr?: number;
+  mb?: number;
+  ml?: number;
+};
+const getMarginStyle = ({ m, mx, my, mt, mr, mb, ml }: GetMarginStyleProps) => {
+  const topMargin = m || my || mt;
+  const rightMargin = m || mx || mr;
+  const bottomMargin = m || my || mb;
+  const leftMargin = m || mx || ml;
+  return {
+    top: topMargin
+      ? getGutterStyle({ position: "top", type: "margin", value: topMargin })
+      : undefined,
+    right: rightMargin
+      ? getGutterStyle({
+          position: "right",
+          type: "margin",
+          value: rightMargin,
+        })
+      : undefined,
+    bottom: bottomMargin
+      ? getGutterStyle({
+          position: "bottom",
+          type: "margin",
+          value: bottomMargin,
+        })
+      : undefined,
+    left: leftMargin
+      ? getGutterStyle({
+          position: "left",
+          type: "margin",
+          value: leftMargin,
+        })
+      : undefined,
+  };
+};
+
+type GetPaddingStyleProps = {
+  p?: number;
+  px?: number;
+  py?: number;
+  pt?: number;
+  pr?: number;
+  pb?: number;
+  pl?: number;
+};
+const getPaddingStyle = ({
+  p,
+  px,
+  py,
+  pt,
+  pr,
+  pb,
+  pl,
+}: GetPaddingStyleProps) => {
+  const topPadding = p || py || pt;
+  const rightPadding = p || px || pr;
+  const bottomPadding = p || py || pb;
+  const leftPadding = p || px || pl;
+  return {
+    top: topPadding
+      ? getGutterStyle({ position: "top", type: "padding", value: topPadding })
+      : undefined,
+    right: rightPadding
+      ? getGutterStyle({
+          position: "right",
+          type: "padding",
+          value: rightPadding,
+        })
+      : undefined,
+    bottom: bottomPadding
+      ? getGutterStyle({
+          position: "bottom",
+          type: "padding",
+          value: bottomPadding,
+        })
+      : undefined,
+    left: leftPadding
+      ? getGutterStyle({
+          position: "left",
+          type: "padding",
+          value: leftPadding,
+        })
+      : undefined,
+  };
+};
+
+export type GetGuttersProps = GetMarginStyleProps & GetPaddingStyleProps;
+export const getGutters = ({
+  p,
+  px,
+  py,
+  pt,
+  pr,
+  pb,
+  pl,
+  m,
+  mx,
+  my,
+  mt,
+  mr,
+  mb,
+  ml,
+}: GetGuttersProps) => {
+  const paddingStyle = getPaddingStyle({ p, px, py, pt, pr, pb, pl });
+  const marginStyle = getMarginStyle({ m, mx, my, mt, mr, mb, ml });
+
+  const paddingDef = Object.values(paddingStyle).filter(Boolean).join("");
+  const marginDef = Object.values(marginStyle).filter(Boolean).join("");
+
+  return `${paddingDef} ${marginDef}`;
+};
