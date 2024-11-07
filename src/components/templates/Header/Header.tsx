ce6beda442b5cb280dc4ec32f46bbd6d226ca294
Ariane Spanneut <aspanneut.pro@gmail.com>
2024-11-07 13:14:17 +0100

    refactor: use imrpoved and new components in header, fix accessibility warning

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -1,58 +1,82 @@
 // == Import : npm
-import React from "react";
+import React, { ReactNode } from "react";
 import { useTranslation } from "react-i18next";
+import { FaBars } from "react-icons/fa";
 
 // == Import : local
-import { useWindowSize } from "@/hooks/window.hook";
-import { Button, Flex, FlexProps, Logo, NavMenuItem } from "../../atoms";
-import { BurgerMenu, MenuItemsProps, NavBar } from "../../molecules";
+import { useWindowSize } from "@/hooks";
+import {
+  Button,
+  ButtonIcon,
+  Flex,
+  FlexProps,
+  Logo,
+  NavMenu,
+  NavMenuItem,
+} from "../../atoms";
+import { useTheme } from "@/theme/theme";
 
+type MenuItemsProps = {
+  id: string;
+  hide?: boolean;
+  content: ReactNode | string;
+  to: string;
+};
 type HeaderProps = {
   logged?: boolean;
   menuItems: MenuItemsProps[];
-  menuItemsGap?: number;
+  onBurgerMenuClick: () => void;
   logOut: () => void;
 } & FlexProps;
 
 export const Header: React.FC<HeaderProps> = ({
   logged,
   menuItems,
-  menuItemsGap,
   logOut,
+  onBurgerMenuClick,
   ...flexProps
 }) => {
   const { isDesktop } = useWindowSize();
   const { t } = useTranslation();
+  const { color } = useTheme();
 
-  return (
+  return isDesktop ? (
     <Flex
       as="header"
       flexDirection="row"
-      justifyContent="space-between"
+      justifyContent={isDesktop ? "space-between" : "end"}
       width="100%"
       py={2}
       {...flexProps}
     >
-      <NavMenuItem id="logo-navbar" to="/">
-        <Logo />
-      </NavMenuItem>
-      {isDesktop ? (
-        <Flex
-          width="100%"
-          flexDirection="row"
-          justifyContent="space-between"
-          as="nav"
-        >
-          <NavBar menuItems={menuItems} gap={menuItemsGap} />
-          {logged && (
-            <Button type="submit" onClick={logOut}>
-              {t("COMMON.LOGOUT")}
-            </Button>
-          )}
-        </Flex>
-      ) : (
-        <BurgerMenu menuItems={menuItems} />
+      <NavMenu gap={30} px={4}>
+        <NavMenuItem to="/" aria-label={`Ofeel logo - ${t("LINK.HOME")}`}>
+          <Logo />
+        </NavMenuItem>
+
+        {menuItems.map(
+          ({ content, hide, id, to }) =>
+            !hide && (
+              <NavMenuItem key={id} to={to}>
+                {content}
+              </NavMenuItem>
+            )
+        )}
+      </NavMenu>
+      {logged && (
+        <Button type="submit" onClick={logOut}>
+          {t("COMMON.LOGOUT")}
+        </Button>
       )}
+    </Flex>
+  ) : (
+    <Flex flexDirection="row" justifyContent="space-between" width="100%">
+      <Logo aria-hidden />
+      <ButtonIcon
+        aria-label={t("COMMON.BURGER_MENU")}
+        icon={<FaBars color={color.yellow.y4} size={50} />}
+        onClick={onBurgerMenuClick}
+      />
     </Flex>
   );
 };
